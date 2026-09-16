ffe87ad6c2cbd7f67eed54a8d7be43291088fba0
Edward123-star <hariharankarthikeyan12.234@gmail.com>
2026-09-16 10:58:13 +0530

    Add basics3 and gitignore

---
+++ Day1/basics3.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class basics3{
+    public static void main(String [] args)
+    {
+        Scanner scan = new Scanner (System.in);
+        System.out.print(" Enter the mark: ");
+        int mark = scan.nextInt();
+        if(mark >= 90)
+        {
+            System.out.println("You got A Grade");
+            System.out.println(" Ypu are Pass");
+        }
+        else if (mark >= 80)
+        {
+            System.out.println("You got B Grade");
+            System.out.println("You are Pass");
+        }
+        else if (mark >= 70)
+        {
+             System.out.println("You got C Grade");
+            System.out.println("You are Pass");
+        }
+        else if (mark >= 60)
+        {
+            System.out.println("You got D Grade");
+            System.out.println("You are Pass");
+        }
+        else
+        {
+            System.out.println("You got F Grade");
+            System.out.println("You are Fail");
+        }
+    }
+}
